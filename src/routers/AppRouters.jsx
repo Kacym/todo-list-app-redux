695c106eaf5ea91358b0fe2269a2e943ee0773b2
Kasym Ulanbekov <ulanbek.kasym@gmail.com>
2023-08-24 11:54:38 +0600

    get-started

---
+++ src/routers/AppRouters.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from 'react-router'
+
+const AppRouters = () => {
+  return (
+    <Routes>
+        
+    </Routes>
+  )
+}
+
+export default AppRouters
